467082beb583da4df4604758746d7125e0d43932
Alexandru Runcan <50112824+IPGG@users.noreply.github.com>
2021-03-26 17:51:06 +0200

    API updates

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const request = require("request");
 const https = require("https");
 
 const app = express();
-// app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -33,11 +32,11 @@ app.post("/", function (req, res) {
     }
 
     var jsonData = JSON.stringify(data);
-    const url = "https://us1.api.mailchimp.com/3.0/lists/47285fadbf";
+    const url = "https://us{Add the server id here}.api.mailchimp.com/3.0/lists/{Add your own KEY ID HERE}";
 
     const options = {
         method: "POST",
-        auth: "CodeKing:c7eb8a8fbbc74d960d9ff2b998bd8118-us1"
+        auth: "{Add your own API ID here}"
     }
 
     const request = https.request(url, options, function(response){
